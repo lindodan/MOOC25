75b288cd10ec400c14849f5fb2488cc2f08e4fc7
dan_desktop <lindodan@fel.cvut.cz>
2025-03-12 11:27:27 +0100

    Finished lesson 8.

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,6 +14,18 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    residual_errors = []
+    for i in range(len(predictions)):
+        residual_errors.append((abs(predictions[i] - net_worths[i]),i))
+
+    residual_errors.sort()
+    for keeping in range(int((len(net_worths)*0.9))):
+        i = residual_errors[keeping][1]
+        error = residual_errors[keeping][0][0]
+        age = ages[i]
+        net_worth = net_worths[i]
+        cleaned_data.append((age,net_worth,error))
+
 
     
     return cleaned_data
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 import joblib
 
 from outlier_cleaner import outlierCleaner
+from sklearn.linear_model import LinearRegression
 
 
 ### load up some practice data with outliers in it
-ages = joblib.load( open("./practice_outliers_ages.pkl", "rb") )
-net_worths = joblib.load( open("./practice_outliers_net_worths.pkl", "rb") )
+ages = joblib.load( open("./practice_outliers_ages_unix.pkl", "rb") )
+net_worths = joblib.load( open("./practice_outliers_net_worths_unix.pkl", "rb") )
 
 
 
@@ -25,7 +26,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print(reg.score(ages_test, net_worths_test))
+print("Regression slope:",reg.coef_)
+print("Regression intercept:",reg.intercept_)
 
 
 
@@ -68,6 +73,10 @@ if len(cleaned_data) > 0:
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print("Regression slope:",reg.coef_)
+        print("Regression intercept:",reg.intercept_)
+        print("Regression score: ",reg.score(ages, net_worths))
+        print("Regression score test: ",reg.score(ages_test, net_worths_test))
     except NameError:
         print("You don't seem to have regression imported/created,")
         print("   or else your regression object isn't named reg")
@@ -76,6 +85,7 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
+
 
 
 else:
