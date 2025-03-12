ad75c7626eb617454d3bc31daa7f9d36eddd4f17
dan_desktop <lindodan@fel.cvut.cz>
2025-03-12 11:29:43 +0100

    SVM,DT,K-NN,BAYES,RANDOM FOREST

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from networkx.algorithms.shortest_paths.unweighted import predecessor
+
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -24,18 +33,58 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+#plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+################ RANDOM FOREST #######################
+"""clf = RandomForestClassifier(n_estimators=50)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print(accuracy_score(labels_test, pred))
+"""
+################### ADABOOST #########################
+clf = AdaBoostClassifier(n_estimators=100, random_state=0,learning_rate=1)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print(accuracy_score(pred, labels_test))
 
 
+###################### K-NN ########################
+"""clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
+print(accuracy_score(pred, labels_test))
+"""
 
+#################### BAYES #########################
+
+"""clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred, labels_test))
+"""
+
+###################### SVM ##########################
+"""clf = SVC(kernel="rbf")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred, labels_test))
+"""
+##################### TREE ########################
+"""clf = DecisionTreeClassifier(min_samples_split=5, random_state=0)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred, labels_test))"""
 
 
 try:
