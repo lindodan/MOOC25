ff08f94f63c2dfd9bafbfcb960d5666d8fa4e944
dan_desktop <lindodan@fel.cvut.cz>
2025-03-13 17:45:30 +0100

    completed final project

--- final_project/tester.py
+++ final_project/tester.py
@@ -27,7 +27,7 @@ RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFals
 def test_classifier(clf, dataset, feature_list, folds = 1000):
     data = featureFormat(dataset, feature_list, sort_keys = True)
     labels, features = targetFeatureSplit(data)
-    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    cv = StratifiedShuffleSplit(labels, random_state = 42).split(features, labels)
     true_negatives = 0
     false_negatives = 0
     true_positives = 0
@@ -81,19 +81,19 @@ DATASET_PICKLE_FILENAME = "my_dataset.pkl"
 FEATURE_LIST_FILENAME = "my_feature_list.pkl"
 
 def dump_classifier_and_data(clf, dataset, feature_list):
-    with open(CLF_PICKLE_FILENAME, "w") as clf_outfile:
+    with open(CLF_PICKLE_FILENAME, "wb") as clf_outfile:
         pickle.dump(clf, clf_outfile)
-    with open(DATASET_PICKLE_FILENAME, "w") as dataset_outfile:
+    with open(DATASET_PICKLE_FILENAME, "wb") as dataset_outfile:
         pickle.dump(dataset, dataset_outfile)
-    with open(FEATURE_LIST_FILENAME, "w") as featurelist_outfile:
+    with open(FEATURE_LIST_FILENAME, "wb") as featurelist_outfile:
         pickle.dump(feature_list, featurelist_outfile)
 
 def load_classifier_and_data():
-    with open(CLF_PICKLE_FILENAME, "r") as clf_infile:
+    with open(CLF_PICKLE_FILENAME, "rb") as clf_infile:
         clf = pickle.load(clf_infile)
-    with open(DATASET_PICKLE_FILENAME, "r") as dataset_infile:
+    with open(DATASET_PICKLE_FILENAME, "rb") as dataset_infile:
         dataset = pickle.load(dataset_infile)
-    with open(FEATURE_LIST_FILENAME, "r") as featurelist_infile:
+    with open(FEATURE_LIST_FILENAME, "rb") as featurelist_infile:
         feature_list = pickle.load(featurelist_infile)
     return clf, dataset, feature_list
 
